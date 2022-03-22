941b64dbae2ad5b3f96516006baaca3da5e34d21
fyupanquia <fyupanquia@outlook.com>
2022-03-21 22:25:56 -0500

    release

---
+++ motion.detector.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+import cv2, time, pandas
+
+first_frame=None
+status_list=[None, None]
+times=[]
+video=cv2.VideoCapture(0)
+df=pandas.DataFrame(columns=["Start", "End"])
+status_str=""
+
+while True:
+    check, frame = video.read()
+    status=0
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray, (21,21), 0)
+
+    if first_frame is None:
+        first_frame=gray
+        continue
+
+    delta_frame=cv2.absdiff(first_frame, gray)
+    thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.dilate(thresh_frame, None, iterations=2)
+
+    contours, hierarchy =cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour)<1000:
+            continue
+        status=1
+
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+
+    status_list.append(status)
+
+    if (status_list[-1] == 1 and status_list[-2] == 0) or (status_list[-1] == 0 and status_list[-2] == 1):
+        times.append(datetime.now())
+
+    cv2.imshow("Gray Frame", gray)
+    cv2.imshow("DeltaFrame Frame", delta_frame)
+    cv2.imshow("Thresh Frame", thresh_frame)
+    cv2.imshow("Color Frame", frame)
+
+    key=cv2.waitKey(1)
+
+    if key==ord("q"):
+        if status==1:
+            times.append(datetime.now())
+        break
+
+    status_str=status_str+str(status)
+    print(status_str)
+
+
+for i in range(0, len(times), 2):
+    df=df.append({"Start":times[i], "End":times[i+1]}, ignore_index=True)
+
+df.to_csv("motion.csv")
+video.release()
+cv2.destroyAllWindows()
